e8caaac7d216865dec5c8a150bdd66829d83e8dc
hongyuewang <hongyuew26@gmail.com>
2020-10-15 10:27:43 -0400

    Create main browser page

---
+++ index.js
@@ -0,0 +1,33 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow } = electron;
+
+function createWindow() {
+  // Create the browser window
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the main html file of the app
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
